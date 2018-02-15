3dd9402ef86a01edd68330686a8114e6b6861f5f
Christian Stolz <hg8496@cstolz.de>
2018-02-16 00:37:17 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # ansible-hcloud-inventory
 An dynamic inventory script for hetzner cloud
+
+Usage:
+`HCLOUD_TOKEN=example ansible-playbook site.yml -u root -i hcloud.py`
+
+Dependencies:
+* requests(`apt install python-request`)
+
+The inventory will consist of multiple groups:
+
+Name | Description
+---- | ----
+all | contains all hosts
+hcloud | contains all hosts in Hetzner Cloud
+fsn1-dc8 | contains all hosts in datacenter Falkenstein
+nbg1-dc3 | contains all hosts in datacenter Nürnberg
+
+The host have the following hostvars 
+
+Name | Description
+---- | ----
+ansible_host | Public IPv4 Adress
+hcloud_server_type | Servertype eg. CX11
+hcloud_image | Name of the used image
+hcloud_datacenter | Datacenter the server is running in
---
+++ hcloud.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import json
+import requests
+import os
+
+def main():
+    api_key = os.environ.get('HCLOUD_TOKEN')
+    hosts = []
+    hostvars = {}
+    root = { 'hetznercloud': {'hosts': hosts}, '_meta': { 'hostvars': hostvars }}
+    url = 'https://api.hetzner.cloud/v1/servers'
+    headers = {'Authorization': 'Bearer ' + api_key}
+
+    r = requests.get(url, headers=headers)
+    for server in r.json()['servers']:
+        server_name = server['name']
+        hosts.append(server_name)
+        hostvars[server_name] = fill_host_vars(server)
+        add_to_datacenter(root, server)
+    print json.dumps(root)
+
+def fill_host_vars(server):
+    return {
+        'ansible_host': server['public_net']['ipv4']['ip'],
+        'hcloud_server_type': server['server_type'],
+        'hcloud_image': server['image']['name'],
+        'hcloud_datacenter': server['datacenter']['name'],
+    }
+
+def add_to_datacenter(root, server):
+    dc = server['datacenter']['name']
+    if not root.has_key(dc):
+        root[dc] = []
+    root[dc].append(server['name'])
+
+if __name__ == '__main__':
+    main()
